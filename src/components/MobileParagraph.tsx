c9ebcca5cf2c183e292dd3be865b0ad108550503
theisolatedmonk <96rahulseth@gmail.com>
2023-05-03 00:51:09 +0530

    Desktop-design -implemented

---
+++ src/components/MobileParagraph.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export  function MobileParagraph() {
+  return (
+    <div>  <div className="flex flex-col w-[480px] text-lg">
+    <p className=" font-bold text-[hsl(271,36%,24%)] text-xl">Simple booking</p>
+    <p className=" text-[hsl(270,7%,64%)] ">
+      {" "}
+      Stay in touch with our dog walkers through the chat interface. This
+      make it easy to discuss arrangements and make bookings. Once the
+      walk has been completed you can rate your walker and book again
+      through the chat.
+    </p>
+  </div></div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,36 +7,28 @@ import dog3 from "@/images/dog-image-3.jpg";
 import {MdNavigateNext} from "react-icons/md"
 import {IoChevronBack} from "react-icons/io"
 import { Mobile } from "@/components/Mobile";
+import { MobileParagraph } from "@/components/MobileParagraph";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen flex justify-center">
-      <div className=" flex justify-between h-full bg-[ hsl(0,0%,100%)] w-full relative">
-        <div className=" w-96 bg-gradient-to-r h-[96%] from-[hsl(264,100%,61%)] to-[hsl(293,100%,63%)]  rounded-br-full"></div>
+    <main className="h-screen w-screen flex justify-center ">
+      <div className=" flex justify-between h-full bg-[ hsl(0,0%,100%)] w-full relative ">
+        <div className=" w-[50vh] h-[96vh] bg-gradient-to-r  from-[hsl(264,100%,61%)] to-[hsl(293,100%,63%)]  rounded-br-full pt-96"></div>
 
         <div className="flex items-end">
-          <div className="w-96 bg-[hsl(270,20%,96%)] h-[96%]    rounded-tl-full "></div>
+          <div className=" w-[50vh] h-[96vh] bg-[hsl(270,20%,96%)]     rounded-tl-full pb-96"></div>
         </div>
       </div>
-    <div className="flex justify-center absolute z-20 items-center  h-full">
+    <div className="flex justify-center absolute z-20 items-center   h-[90] ">
     <div
-        className="flex  h-screen gap-20 w-screen items-center justify-center  "
+        className="flex    gap-44 w-screen items-center justify-center mt-10"
       >
         <Mobile></Mobile>
-        <div className="flex flex-col w-72 text-xs">
-          <p className=" font-bold text-[hsl(271,36%,24%)]">Simple booking</p>
-          <p className=" text-[hsl(270,7%,64%)] ">
-            {" "}
-            Stay in touch with our dog walkers through the chat interface. This
-            make it easy to discuss arrangements and make bookings. Once the
-            walk has been completed you can rate your walker and book again
-            through the chat.
-          </p>
-        </div>
+        <MobileParagraph/>
       </div>
     </div>
     </main>
   );
-}
+}   
